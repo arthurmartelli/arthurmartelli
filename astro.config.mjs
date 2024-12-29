1abc2e3eb3e253e1adb39941102d7e51f23aab9b
Arthur Martelli <arthur.martellig@gmail.com>
2024-12-28 18:12:24 -0600

    feat: added tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
     output: "static",
     outDir: "./dist",
     compressHTML: true,
+    vite: {
+        plugins: [ tailwindcss() ]
+    },
 
     image: { domains: ["images.unsplash.com"] },
     prefetch: { defaultStrategy: 'viewport' },
